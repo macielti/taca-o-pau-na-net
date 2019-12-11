e56d7ea89588196a82d493ad81fbdbdd78210d64
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2019-12-11 08:42:59 -0300

    measure internet speed

--- source/main.py
+++ source/main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import messagebox
+import utils
 
 # main frame
 root = tk.Tk()
@@ -23,13 +25,34 @@ passwd_entry.grid(row=2, column=1)
 tk.Label(root, text="Dados do provedor").grid(row=3, column=0, columnspan=2, ipady=10)
 
 # entry for internet provider instagram username
-tk.Label(root, text="Login do Provedor:").grid(row=4, column=0)
+tk.Label(root, text="Instagram do Provedor:").grid(row=4, column=0)
 provider_login_var = tk.StringVar()
 provider_login_entry = tk.Entry(root, textvariable=provider_login_var)
 provider_login_entry.grid(row=4, column=1)
 
+# entry for velocity contract
+tk.Label(root, text="Velocidade contratadas em Megas:").grid(row=5, column=0)
+velocity_var = tk.StringVar()
+velocity_entry = tk.Entry(root, textvariable=velocity_var)
+velocity_entry.grid(row=5, column=1)
+
 # button to make the reclamation
-complain_button = tk.Button(root, text="Analisar e Reclamar")
-complain_button.grid(row=5, column=0, columnspan=2, ipadx=50, pady=20)
+def calculate():
+    global velocity_var
+    
+    result = utils.measure()
+
+    down_contract = velocity_var.get()
+    down_shiped = result['download']/1000000
+
+    message_box_str = "Velocidade de download entregue: %.2f Megas\n Velocidade de download contratada: %.2f Megas\n O provedor está entregando %.2f %% do que foi contratado." % (down_shiped, float(down_contract), (100*down_shiped)/float(down_contract))
+    messagebox.showinfo("Resultados - <Provedor>", message_box_str)
+
+complain_button = tk.Button(root, text="Analisar e Reclamar", command=calculate)
+complain_button.grid(row=6, column=0, columnspan=2, ipadx=50, pady=20)
+
+# show results
+#messagebox.showinfo(
+#        "Gabarito: ( ͡╥ ᖨ ͡╥)", "O seu Gabarito está triste pois não deram um nome para ele.")
 
 root.mainloop()
---
+++ source/utils.py
@@ -0,0 +1,14 @@
+import speedtest
+
+def measure():
+    s = speedtest.Speedtest()
+    s.upload(pre_allocate=False)
+    #s.get_servers(servers)
+    s.get_best_server()
+    s.download(threads=1)
+
+    results_dict = s.results.dict()
+    return results_dict
+
+if __name__ == "__main__":
+    print(measure())
